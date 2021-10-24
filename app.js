608734b5a7f51f0bffaca94e4e792d01b8d1bd97
Fandaxd <finczee@seznam.cz>
2021-10-24 22:44:14 +0200

    added ui

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+let losses = 0
+let wins = 0
+const container = document.querySelector(".container")
+
+const rockBtn = document.createElement("button")
+rockBtn.innerHTML = "rock"
+rockBtn.addEventListener("click", () => oneRound("rock", computerPlay()))
+container.appendChild(rockBtn)
+
+const paperBtn = document.createElement("button")
+paperBtn.innerHTML = "paper"
+paperBtn.addEventListener("click", () => oneRound("paper", computerPlay()))
+container.appendChild(paperBtn)
+
+const scissorsBtn = document.createElement("button")
+scissorsBtn.innerHTML = "scissors"
+scissorsBtn.addEventListener("click", () => oneRound("scissors", computerPlay()))
+container.appendChild(scissorsBtn)
+
+const scoreDiv = document.createElement("div")
+scoreDiv.innerHTML = `Player score: ${wins} <br> Computer score: ${losses}`
+container.appendChild(scoreDiv)
+
+const resultsDiv = document.createElement("div")
+container.appendChild(resultsDiv)
+
+
+
 const computerPlay = () => {
     let randomNumber = Math.floor(Math.random() * 3)
     // console.log(randomNumber)
@@ -7,55 +35,47 @@ const computerPlay = () => {
 }
 
 // console.log(computerPlay())
-let losses = 0
-let wins = 0
-const oneRound = (playerSelection, computerSelection) => {
-    
-    
-    
-    if(playerSelection.toLowerCase() === computerSelection) console.log("It's a tie!")
-        else if(playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
+
+const oneRound = (playerSelection, computerSelection) => {    
+    if(playerSelection === computerSelection) {}
+        else if(playerSelection === "rock" && computerSelection === "paper") {
             losses++
-            console.log("You lose! Paper beats Rock")
         }
-        else if(playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+        else if(playerSelection === "rock" && computerSelection === "scissors") {
             wins++
-            console.log("You win! Rock beats Scissors")
         }
-        else if(playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+        else if(playerSelection === "paper" && computerSelection === "rock") {
             wins++
-            console.log("You win! Paper beats Rock")
         }
-        else if(playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
+        else if(playerSelection === "paper" && computerSelection === "scissors") {
             losses++
-            console.log("You lose! Scissors beat paper")
         }
-        else if(playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+        else if(playerSelection === "scissors" && computerSelection === "paper") {
             wins++
-            console.log("You win! Scissors beat paper")
         }
         else {
             losses++
-            console.log("You lose. Rock beats scissors")
         }
-    
+    scoreDiv.innerHTML = `Player score: ${wins} <br> Computer score: ${losses}`
+    if(wins > 4) resultsDiv.innerHTML = "<h1>You Won</h1>"
+        else if(losses > 4) resultsDiv.innerHTML = "<h1>You Lost</h1>"
 }
 
 
 
-const game = () => {
-    for(let i = 0; i < 5; i++){
-        const playerSelection = prompt('Type here "rock", "paper" or "scissors"')
-        const computerSelection = computerPlay()
-        console.log(`player is ${playerSelection}`)
-        console.log(`comp is ${computerSelection}`)
-        // console.log(oneRound(playerSelection, computerSelection))
+// const game = () => {
+//     for(let i = 0; i < 5; i++){
+//         const playerSelection = prompt('Type here "rock", "paper" or "scissors"')
+//         const computerSelection = computerPlay()
+//         console.log(`player is ${playerSelection}`)
+//         console.log(`comp is ${computerSelection}`)
+//         // console.log(oneRound(playerSelection, computerSelection))
 
-        oneRound(playerSelection, computerSelection)
-        console.log(wins)
-        console.log(losses)
-    }
-    alert(`won: ${wins}, lost: ${losses}`)
-}
+//         oneRound(playerSelection, computerSelection)
+//         console.log(wins)
+//         console.log(losses)
+//     }
+//     alert(`won: ${wins}, lost: ${losses}`)
+// }
 
-game()
+// game()
